0ce427d86365316b800c431fe1916506608ec085
Victor Oliveira <vimatozin@hotmail.com>
2021-09-26 11:10:48 -0300

    First commit about angular

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'', pathMatch:'full', redirectTo:'courses'}, //estou dizendo aqui que se a URL estiver vazia, iremos carregar a url /courses automaticamente
+  {
+    path:'courses',
+   loadChildren: () => import('./courses/courses.module').then(m => m.CoursesModule)
+  }
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
