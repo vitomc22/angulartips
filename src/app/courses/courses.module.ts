411203a7a01edf228a066c7da925933aeb954798
Victor Oliveira <vimatozin@hotmail.com>
2021-10-04 19:28:47 -0300

    Organizando material componentes em uma pasta

--- src/app/courses/courses.module.ts
+++ src/app/courses/courses.module.ts
@@ -1,9 +1,10 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
-import {MatTableModule} from '@angular/material/table';
 
+import { AppMaterialModule } from './../shared/app-material/app-material.module';
 import { CoursesRoutingModule } from './courses-routing.module';
 import { CoursesComponent } from './courses/courses.component';
+
 
 
 @NgModule({
@@ -13,8 +14,8 @@ import { CoursesComponent } from './courses/courses.component';
   imports: [
     CommonModule,
     CoursesRoutingModule, //módulo de roteamento do angular
-    MatTableModule
+    AppMaterialModule
   ],
-  exports:[MatTableModule]
+
 })
 export class CoursesModule { }
