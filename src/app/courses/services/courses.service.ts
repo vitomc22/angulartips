485c8e4b20b8e358c06600846b75ce1e8149062e
Victor Oliveira <vimatozin@hotmail.com>
2021-10-18 20:39:19 -0300

    Adicionando observable

--- src/app/courses/services/courses.service.ts
+++ src/app/courses/services/courses.service.ts
@@ -1,18 +1,20 @@
 import { Course } from './../model/course';
 import { Injectable } from '@angular/core';
 import {HttpClient} from '@angular/common/http';
-
+import { first, tap } from 'rxjs/operators';
 @Injectable({
   providedIn: 'root'
 })
 export class CoursesService {
 
+  private readonly API = '/assets/courses.json';
 
   constructor(private httpClient: HttpClient) { }
 
-  list(): Course []{
-    return  [
-      {  _id:'1', name:'Angular', category:'front-end'  }
-    ];
+  list() {
+    return  this.httpClient.get<Course[]>(this.API) //Criamos um observable, com generics do tipo Course que é lista
+    .pipe(
+      first(), //somente pegar o dado uma fez
+      tap(courses => console.log(courses))); //pipe para coleta dos dados e tap para usar log de forma reativa
   }
 }
