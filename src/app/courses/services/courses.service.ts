053d6036472ae7bfe6983b718fb932ab59dcaeeb
Victor Oliveira <vimatozin@hotmail.com>
2021-11-11 08:32:52 -0300

    Utilizando pipe para troca de icones

--- src/app/courses/services/courses.service.ts
+++ src/app/courses/services/courses.service.ts
@@ -15,7 +15,7 @@ export class CoursesService {
     return  this.httpClient.get<Course[]>(this.API) //Criamos um observable, com generics do tipo Course que é lista
     .pipe(
       first(), //somente pegar o dado uma fez
-      delay(5000), //atrasando o recebimento de proposito para testar o spinner
+      //atrasando o recebimento de proposito para testar o spinner
       tap(courses => console.log(courses))); //pipe para coleta dos dados e tap para usar log de forma reativa
   }
 }
--- src/app/shared/app-material/app-material.module.ts
+++ src/app/shared/app-material/app-material.module.ts
@@ -3,8 +3,9 @@ import { MatCardModule } from '@angular/material/card';
 import { MatTableModule } from '@angular/material/table';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
-import {MatDialogModule} from '@angular/material/dialog';
-import {MatButtonModule} from '@angular/material/button';
+import { MatDialogModule } from '@angular/material/dialog';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
 
 @NgModule({
   exports: [
@@ -13,7 +14,8 @@ import {MatButtonModule} from '@angular/material/button';
     MatToolbarModule,
     MatProgressSpinnerModule,
     MatDialogModule,
-    MatButtonModule
+    MatButtonModule,
+    MatIconModule,
   ],
 })
 export class AppMaterialModule {}
