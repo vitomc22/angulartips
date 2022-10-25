32212ded714b9d36372205b31a2af54ba64294e0
Victor Oliveira <vimatozin@hotmail.com>
2022-10-24 22:13:07 -0300

    criado metodo PUT

--- src/app/courses/containers/course-form/course-form.component.ts
+++ src/app/courses/containers/course-form/course-form.component.ts
@@ -1,11 +1,11 @@
+import { Course } from './../../model/course';
 import { Location } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
 import { NonNullableFormBuilder } from '@angular/forms';
 import { MatSnackBar } from '@angular/material/snack-bar';
+import { ActivatedRoute } from '@angular/router';
 
 import { CoursesService } from './../../services/courses.service';
-
-
 
 @Component({
   selector: 'app-course-form',
@@ -15,18 +15,27 @@ import { CoursesService } from './../../services/courses.service';
 export class CourseFormComponent implements OnInit {
   //declarando e inicializando tipo de formulario
   form = this.formBuilder.group({
+    _id: [''],
     name: [''],
-    category: [''],
+    category: ['']
   });
 
   constructor(
     private formBuilder: NonNullableFormBuilder,
     private service: CoursesService,
     private _snackBar: MatSnackBar,
-    private location: Location
+    private location: Location,
+    private route: ActivatedRoute
   ) {}
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    const course: Course = this.route.snapshot.data['course']; 
+    this.form.setValue({
+      _id: course._id,
+      name: course.name,
+      category: course.category
+    });
+  }
 
   onSubmit() {
     this.service.save(this.form.value).subscribe(
--- src/app/courses/services/courses.service.ts
+++ src/app/courses/services/courses.service.ts
@@ -5,28 +5,40 @@ import { first, tap } from 'rxjs/operators';
 import { Course } from './../model/course';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class CoursesService {
-
   private readonly API = 'api/courses';
 
-  constructor(private httpClient: HttpClient) { }
+  constructor(private httpClient: HttpClient) {}
 
   list() {
-    return  this.httpClient.get<Course[]>(this.API) //Criamos um observable, com generics do tipo Course que é lista
-    .pipe(
-      first(), //somente pegar o dado uma fez
-      //atrasando o recebimento de proposito para testar o spinner
-      tap(courses => console.log(courses))); //pipe para coleta dos dados e tap para usar log de forma reativa
+    return this.httpClient
+      .get<Course[]>(this.API) //Criamos um observable, com generics do tipo Course que é lista
+      .pipe(
+        first(), //somente pegar o dado uma fez
+        //atrasando o recebimento de proposito para testar o spinner
+        tap((courses) => console.log(courses))
+      ); //pipe para coleta dos dados e tap para usar log de forma reativa
   }
 
-  save(record: Partial<Course>){ //partial significa que aceito o objeto incompleto, sem todos os atributos, tipo um DTO do java
-    return this.httpClient.post<Course>(this.API,record).pipe(first());   
+  save(record: Partial<Course>) {
+    //partial significa que aceito o objeto incompleto, sem todos os atributos, tipo um DTO do java
+    if(record._id){
+      return this.update(record);
+    }
+    return this.create(record);
   }
 
-  loadByid(id: string){
+  private create(record: Partial<Course>) {
+    return this.httpClient.post<Course>(this.API, record).pipe(first());
+  }
+
+  private update(record: Partial<Course>) {
+    return this.httpClient.put<Course>(`${this.API}/${record._id}`, record).pipe(first());
+  }
+
+  loadByid(id: string) {
     return this.httpClient.get<Course>(`${this.API}/${id}`);
   }
-
 }
